e13692dc4eac30e21a392995f0dcee7d02f0f8bd
jacky <yaacovisraelnathan@gmail.com>
2016-11-23 20:06:56 +0200

    add methode to get the riht direction for geting the geocoordination of the house

--- public/javascripts/House.js
+++ public/javascripts/House.js
@@ -116,6 +116,7 @@ app.controller('house', ['$scope', '$http', '$state', '$interval', '$mdDialog', 
                     $scope.treatment_fees = response.data.house.treatment_fees;
                     $scope.renovation_fees = response.data.house.renovation_fees;
                     $scope.divers_fees = response.data.house.divers_fees;
+                    newMapLocation($scope.house_number, $scope.street, $scope.city, $scope.state);
                 },
                 function error(response) {
                     showAlert("Your attention please", response.data, "cant load houses");
@@ -160,111 +161,99 @@ app.controller('house', ['$scope', '$http', '$state', '$interval', '$mdDialog', 
     }
 
     var results = {
-                     "results" : [
-                        {
-                           "address_components" : [
-                              {
-                                 "long_name" : "283",
-                                 "short_name" : "283",
-                                 "types" : [ "street_number" ]
-                              },
-                              {
-                                 "long_name" : "Prince Street",
-                                 "short_name" : "Prince St",
-                                 "types" : [ "route" ]
-                              },
-                              {
-                                 "long_name" : "Springfield/Belmont",
-                                 "short_name" : "Springfield/Belmont",
-                                 "types" : [ "neighborhood", "political" ]
-                              },
-                              {
-                                 "long_name" : "Newark",
-                                 "short_name" : "Newark",
-                                 "types" : [ "locality", "political" ]
-                              },
-                              {
-                                 "long_name" : "Essex County",
-                                 "short_name" : "Essex County",
-                                 "types" : [ "administrative_area_level_2", "political" ]
-                              },
-                              {
-                                 "long_name" : "New Jersey",
-                                 "short_name" : "NJ",
-                                 "types" : [ "administrative_area_level_1", "political" ]
-                              },
-                              {
-                                 "long_name" : "États-Unis",
-                                 "short_name" : "US",
-                                 "types" : [ "country", "political" ]
-                              },
-                              {
-                                 "long_name" : "07108",
-                                 "short_name" : "07108",
-                                 "types" : [ "postal_code" ]
-                              },
-                              {
-                                 "long_name" : "2629",
-                                 "short_name" : "2629",
-                                 "types" : [ "postal_code_suffix" ]
-                              }
-                           ],
-                           "formatted_address" : "283 Prince St, Newark, NJ 07108, États-Unis",
-                           "geometry" : {
-                              "location" : {
-                                 "lat" : 40.7269049,
-                                 "lng" : -74.1896381
-                              },
-                              "location_type" : "ROOFTOP",
-                              "viewport" : {
-                                 "northeast" : {
-                                    "lat" : 40.7282538802915,
-                                    "lng" : -74.18828911970849
-                                 },
-                                 "southwest" : {
-                                    "lat" : 40.7255559197085,
-                                    "lng" : -74.19098708029151
-                                 }
-                              }
-                           },
-                           "partial_match" : true,
-                           "place_id" : "ChIJCcsncmtTwokR_9zF-itQG94",
-                           "types" : [ "street_address" ]
-                        }
-                     ],
-                     "status" : "OK"
-                  }
-var latitude = results.results[0].geometry.location.lat;
-var longitude = results.results[0].geometry.location.lng;
-
-    var uluru = {
-        lat: latitude,
-        lng: longitude
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+        "results": [{
+            "address_components": [{
+                "long_name": "283",
+                "short_name": "283",
+                "types": ["street_number"]
+            }, {
+                "long_name": "Prince Street",
+                "short_name": "Prince St",
+                "types": ["route"]
+            }, {
+                "long_name": "Springfield/Belmont",
+                "short_name": "Springfield/Belmont",
+                "types": ["neighborhood", "political"]
+            }, {
+                "long_name": "Newark",
+                "short_name": "Newark",
+                "types": ["locality", "political"]
+            }, {
+                "long_name": "Essex County",
+                "short_name": "Essex County",
+                "types": ["administrative_area_level_2", "political"]
+            }, {
+                "long_name": "New Jersey",
+                "short_name": "NJ",
+                "types": ["administrative_area_level_1", "political"]
+            }, {
+                "long_name": "États-Unis",
+                "short_name": "US",
+                "types": ["country", "political"]
+            }, {
+                "long_name": "07108",
+                "short_name": "07108",
+                "types": ["postal_code"]
+            }, {
+                "long_name": "2629",
+                "short_name": "2629",
+                "types": ["postal_code_suffix"]
+            }],
+            "formatted_address": "283 Prince St, Newark, NJ 07108, États-Unis",
+            "geometry": {
+                "location": {
+                    "lat": 40.7269049,
+                    "lng": -74.1896381
+                },
+                "location_type": "ROOFTOP",
+                "viewport": {
+                    "northeast": {
+                        "lat": 40.7282538802915,
+                        "lng": -74.18828911970849
+                    },
+                    "southwest": {
+                        "lat": 40.7255559197085,
+                        "lng": -74.19098708029151
+                    }
+                }
+            },
+            "partial_match": true,
+            "place_id": "ChIJCcsncmtTwokR_9zF-itQG94",
+            "types": ["street_address"]
+        }],
+        "status": "OK"
+    }
 
 
-function newMapLocation(nNumberOfHouse,szStreetName,szCityName,szStateName)
-{
+    function newMapLocation(nNumberOfHouse, szStreetName, szCityName, szStateName) {
+        var longitude = 0.0;
+        var latitude = 0.0;
 
- // Get information conserning the house
-        $http.get("/maps.googleapis.com/maps/api/geocode/json?address=" +nNumberOfHouse+"+,"+szStreetName+"+,"+szCityName+"+,"+szStateName)
+        // Get information conserning the house
+        $http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + nNumberOfHouse + "," + szStreetName + "," + szCityName + "," + szStateName + "&key=AIzaSyCOx3ErCzTt9zayUQluJsKo_Gpnp4ztMbI")
             .then(function successCallback(response) {
-
+                    latitude = response.data.results[0].geometry.location.lat;
+                    longitude = response.data.results[0].geometry.location.lng;
+                     var uluru = {
+                                lat: latitude,
+                                lng: longitude
+                            };
+                            var map = new google.maps.Map(document.getElementById('map'), {
+                                zoom: 15,
+                                center: uluru
+                            });
+                            var marker = new google.maps.Marker({
+                                position: uluru,
+                                map: map
+                            });
                 },
                 function error(response) {
                     showAlert("Your attention please", response.data, "cant load maps");
                 });
 
-//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDpC9Z3zA73VG-FgbPdijlztIkOjrq8mAI
-//https://maps.googleapis.com/maps/api/geocode/json?address="nNumberOfHouse+szStreetName+szCityName,+szStateName&key=AIzaSyDpC9Z3zA73VG-FgbPdijlztIkOjrq8mAI;
-}
+
+
+
+    }
 
 }]);
