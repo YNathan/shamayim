3c9c9efb4062e760eacc3d9a2b0c6f9bdf4467ef
jacky <yaacovisraelnathan@gmail.com>
2016-11-24 15:37:36 +0200

    test for xml

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,8 +1,16 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.*;
 
@@ -16,7 +24,6 @@ import play.libs.F;
 import play.libs.F.*;
 
 import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
 
 
 /**
@@ -27,18 +34,49 @@ import static org.fest.assertions.Assertions.*;
 */
 public class ApplicationTest {
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
 
     @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
+    public void readXml() {
+        try {
+
+            File fXmlFile = new File("../app/Entity/Language/Language.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("hebrew");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("house_id : " + eElement.getAttribute("house_id"));
+                    System.out.println("state : " + eElement.getElementsByTagName("state").item(0).getTextContent());
+                    System.out.println("city : " + eElement.getElementsByTagName("city").item(0).getTextContent());
+                    System.out.println("street : " + eElement.getElementsByTagName("street").item(0).getTextContent());
+                    System.out.println("house_number : " + eElement.getElementsByTagName("house_number").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 
 
 }
